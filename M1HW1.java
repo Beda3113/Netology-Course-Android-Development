f221800860b4635306b36c8d134323beedee050b
Anton Petrov <130255340+Beda3113@users.noreply.github.com>
2026-07-31 18:07:51 +0300

    Update M1HW1.java

--- M1HW1.java
+++ M1HW1.java
@@ -1,6 +1,6 @@
-public class M1HW1 {
+public class Main {
     public static void main(String[] args) {
-        // Вывод имени и фамилии на латинице
+       
         System.out.println("Anton Petrov");
     }
 }
